dafaedd7c8b78340b7153ed7734e80e1db1d91fd
noor-e-alam <noor-e-alam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-25 22:08:58 +0000

    updating ...
    git-svn-id: https://plugins.svn.wordpress.org/amazing-hover-effects/trunk@972685 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/metabox/effects.php
+++ admin/metabox/effects.php
@@ -13,6 +13,13 @@ return array(
 		
 		'fields' => array(				
 
+		array(
+    'type' => 'notebox',
+    'name' => 'nb_1',
+    'label' => __('An Announcement', 'vp_textdomain'),
+    'description' => __('Thanks for installing my plugin. If you need any customization or extra features within this plugin and any other wordpress work, I am available for HIRE at <a href="https://www.odesk.com/users/%7E016f8320a8ec7f966f">Odesk.com</a>', 'vp_textdomain'),
+    'status' => 'info',
+    ),
 
 
 				array(
