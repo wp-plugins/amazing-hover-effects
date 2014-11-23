710c22d7c6aa12ced2d3698899fcd4ce552a43c1
noor-e-alam <noor-e-alam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-23 00:36:19 +0000

    files updated
    git-svn-id: https://plugins.svn.wordpress.org/amazing-hover-effects/trunk@1030958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/metabox/main.php
+++ admin/metabox/main.php
@@ -18,7 +18,7 @@ return array(
     'type' => 'notebox',
     'name' => 'nb_1',
     'label' => __('Author Comment', 'vp_textdomain'),
-    'description' => __('If you want to enable this awesome features, you need to buy pro version from here <a href="http://www.bolobd.com/plugins/amazing-hover-effects/">Amazing Hover Effects Pro</a>', 'vp_textdomain'),
+    'description' => __('If you want to enable this awesome features, you need to buy pro version from here <a href="http://demo.wpvalueclub.com/amazing-hover-effects/">Amazing Hover Effects Pro</a>', 'vp_textdomain'),
     'status' => 'error',
     ),
 	
